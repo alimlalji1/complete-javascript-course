b3801dd2431eac58964298c7fce1fdbcf4d1cc4f
Alim Lalji <alimlalji1@gmail.com>
2020-07-09 13:05:54 +0100

    8. Variables and Data Types - Complete

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,19 @@
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+// To show data type undefined - a variable with no value/non-existent value assosciated with it
+var job;
+console.log(job);
+
+job = 'Teacher'
+console.log(job);
+
+
+// All variable names must start with $, _ or a letter
+
