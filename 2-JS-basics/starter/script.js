d860feed00d90c1a60ffafcb5eaf23278f3ce445
Alim Lalji <alimlalji1@gmail.com>
2020-07-13 22:57:10 +0100

    16. Ternary Operators & Switch Elements - Complete

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -163,3 +163,135 @@ console.log("Is Mark's BMI higher than John's?" + " " + markHigherBMI);
 /*****************************
 * 14. If/Else Statements
 */
+
+/*
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+  console.log(firstName + ' is married!')
+} else {
+  console.log(firstName + ' will hopefully marry soon :)');
+};
+
+var isMarried = true;
+if (isMarried) {
+  console.log(firstName + ' is married!')
+} else {
+  console.log(firstName + ' will hopefully marry soon :)');
+};
+
+
+var massMark = 50;
+var heightMark = 1.5;
+var BMIMark = massMark/(heightMark * heightMark);
+console.log(BMIMark); // 22.2
+
+var massJohn = 75;
+var heightJohn = 1.75;
+var BMIJohn = massJohn/(heightJohn * heightJohn);
+console.log(BMIJohn); // 24.5
+
+if (BMIMark > BMIJohn) {
+  console.log("Mark's BMI is higher than John's");
+} else {
+  console.log("John's BMI is higher than Mark's");
+};
+*/
+
+/*****************************
+* 15. Boolean Logic
+*/
+
+/*
+var firstName = 'John';
+var age = 20;
+
+if (age < 13) {
+  console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) { // Between 13 and 20
+  console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+  console.log(firstName + ' is a young man.');
+} else  {
+  console.log(firstName + ' is a man.');
+};
+*/
+
+/*****************************
+* 16. Ternary Operators & Switch Elements
+*/
+
+/*
+var firstName = 'John';
+var age = 22;
+
+// Ternary Operator
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer': 'juice';
+console.log(drink);
+*/
+
+/* Alternative
+if (age >= 18) {
+  var drink = 'beer';
+} else {
+  var drink = 'juice';
+}
+*/
+
+/*
+// Switch Statement
+var job = 'instructor';
+switch (job) {
+  case 'teacher':
+  case 'instructor':
+    console.log(firstName + ' teaches kids how to code.');
+    break;
+  case 'driver':
+    console.log(firstName + ' drives an uber in Lisbon.');
+    break;
+  case 'designer':
+    console.log(firstName + ' designs beautiful websites.');
+    break;
+  default:
+    console.log(firstName + ' does something else.');
+}
+*/
+
+// Another Example of Switch Statement
+
+// Objective: Rewrite the following using switch statement
+/*if (age < 13) {
+  console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) { // Between 13 and 20
+  console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+  console.log(firstName + ' is a young man.');
+} else  {
+  console.log(firstName + ' is a man.');
+};
+*/
+
+/*
+// Want to compare each of expressions below to true
+age = 17;
+switch (true) {
+  case age < 13:
+    console.log(firstName + ' is a boy.');
+    break;
+  case age >= 13 && age < 20:
+    console.log(firstName + ' is a teenager.');
+    break;
+  case age >= 20 && age < 30:
+    console.log(firstName + ' is a young man.');
+    break;
+  default:
+    console.log(firstName + ' is a man.');
+};
+*/
+
+/*****************************
+* 17. Truthy & Falsy Values and Equality Operators
+*/
